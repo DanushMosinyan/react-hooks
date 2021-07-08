2312de7741fb66069fa723d58ba7c9927eb1d4bf
DanushMosinyan <67325516+DanushMosinyan@users.noreply.github.com>
2021-07-08 19:10:30 +0400

    deleting

--- src/Context/CardsColorContext.js
+++
@@ -1,7 +0,0 @@
-import { useContext, createContext } from "react";
-
-export const CardsColorContext = createContext();
-
-export const useCardsColorContext = () => {
-  return useContext(CardsColorContext);
-};
